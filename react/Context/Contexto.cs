68e35898060f0a1d467b10b5482ad91ff6c4a648
Gabriel Alves <alvesg2003@gmail.com>
2023-02-02 02:06:25 -0300

    first commit

---
+++ react/Context/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using react.Model;
+
+namespace react.Context
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options) :base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ react/Model/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace react.Model
+{
+    [Table("Produto")]
+    public class Produto
+    {
+
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        public string? Nome { get; set; }
+    }
+}
